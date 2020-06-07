c2e9e210c40262b782573337aa5e39fdba740425
Demola Malomo <malomo.alaba@yahoo.com>
2020-06-07 01:28:18 +0100

    project setup with tailwindcss, postcss and typescript

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+import React from 'react'
+
+const Home = () => {
+	return (
+		<div className='font-black'>
+			Hello World
+		</div>
+	)
+}
+
+export default Home
+
+
